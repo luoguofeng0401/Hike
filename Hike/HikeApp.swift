146cff3687a0f2ac475db7df49e7ea5ce6c16c2c
Leo <luoguofeng0401@gmail.com>
2026-06-11 08:05:18 +0800

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Guofeng Luo on 2026-06-11.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
