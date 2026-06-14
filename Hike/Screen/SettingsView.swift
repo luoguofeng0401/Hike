5d1f13c6774162abfabdc927f0f14a8dcad38082
Leo <luoguofeng0401@gmail.com>
2026-06-14 11:31:02 +0800

    7

--- Hike/Screen/SettingsView.swift
+++ Hike/Screen/SettingsView.swift
@@ -10,22 +10,49 @@ import SwiftUI
 struct SettingsView: View {
     var body: some View {
         List {
-            
-            HStack {
-                Spacer()
+            Section {
+                HStack {
+                    Spacer()
+                    
+                    Image(systemName: "laurel.leading")
+                        .font(.system(size: 80, weight: .black))
+                    
+                    VStack(spacing: -10) {
+                        Text("Hike")
+                            .font(.system(size: 66, weight: .black))
+                        
+                        Text("Editors' Choice")
+                            .fontWeight(.medium)
+                    }
+                    
+                    Image(systemName: "laurel.trailing")
+                        .font(.system(size: 80, weight: .black))
+                    
+                    Spacer()
+                }
+                .foregroundStyle(
+                    LinearGradient(colors: [.customGreenLight, .customGreenMedium, .customGreenDark], startPoint: .top, endPoint: .bottom)
+                )
+                .padding(.top, 8)
                 
-                Image(systemName: "laurel.leading")
-                    .font(.system(size: 80, weight: .black))
-                
-                Text("Hike")
-                    .font(.system(size: 66, weight: .black))
-                
-                Image(systemName: "laurel.trailing")
-                    .font(.system(size: 80, weight: .black))
-                
-                Spacer()
+                VStack(spacing: 8) {
+                    Text("Where can you find \nperfect tracks?")
+                        .font(.title2)
+                        .fontWeight(.heavy)
+                    
+                    Text("The hike which looks gorgeous in photos but is even better once you are actually there. The hike that you hope to do again someday. \nFind the best day hikes in the app.")
+                        .font(.footnote)
+                        .italic()
+                    
+                    Text("Dust off the boots! It's time for a walk.")
+                        .fontWeight(.heavy)
+                        .foregroundColor(.customGreenMedium)
+                        .frame(maxWidth: .infinity)
+                }
+                .multilineTextAlignment(.center)
+                .padding(.bottom, 16)
             }
-            
+            .listRowSeparator(.hidden)
         }
     }
 }
