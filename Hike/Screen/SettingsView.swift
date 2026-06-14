872ea4889c312e45aa2d34f0d8318d542dfb3423
Leo <luoguofeng0401@gmail.com>
2026-06-14 13:01:52 +0800

    8

--- Hike/Screen/SettingsView.swift
+++ Hike/Screen/SettingsView.swift
@@ -8,6 +8,15 @@
 import SwiftUI
 
 struct SettingsView: View {
+    
+    private let alternateAppIcons: [String] = [
+        "AppIcon-MagnifyingGlass",
+        "AppIcon-Map",
+        "AppIcon-Mushroom",
+        "AppIcon-Camera",
+        "AppIcon-Backpack",
+        "AppIcon-Campfire"
+    ]
     var body: some View {
         List {
             Section {
@@ -51,6 +60,43 @@ struct SettingsView: View {
                 }
                 .multilineTextAlignment(.center)
                 .padding(.bottom, 16)
+            }
+            .listRowSeparator(.hidden)
+            
+            Section(header: Text("Alternate")) {
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing:12) {
+                        ForEach(alternateAppIcons.indices, id: \.self) { item in
+                            Button {
+                                print("ICon \(alternateAppIcons[item]) was pressed.")
+                                
+                                UIApplication.shared.setAlternateIconName(alternateAppIcons[item]) {
+                                    error in
+                                    if error != nil {
+                                        print("Failed request to update the app's icon: \(String(describing: error?.localizedDescription))")
+                                    } else {
+                                        print("Success! You have changed the app's icon to \(alternateAppIcons[item])")
+                                    }
+                                }
+                            } label: {
+                                Image("\(alternateAppIcons[item])-Preview")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 80, height: 80)
+                                    .cornerRadius(16)
+                            }
+                            .buttonStyle(.borderless)
+                        }
+                    }
+                }
+                .padding(.top, 12)
+                
+                Text("Choose your favourite app icon from the collection above.")
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.secondary)
+                    .font(.footnote)
+                    .padding(.bottom, 12)
             }
             .listRowSeparator(.hidden)
             
