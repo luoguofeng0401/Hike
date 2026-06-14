5aa3735a10648ee019c69052f1338f800c6d0d06
Leo <luoguofeng0401@gmail.com>
2026-06-14 11:05:45 +0800

    6

---
+++ Hike/Screen/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Hike
+//
+//  Created by Guofeng Luo on 2026-06-14.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Setting View")
+    }
+}
+
+#Preview {
+    SettingsView()
+}
